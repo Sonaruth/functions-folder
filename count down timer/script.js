60089994fd63fed2fbad364323a0abd7592ad6e5
sonaruth <sonaruth20@gmail.com>
2024-07-15 00:06:11 +0100

    first commit

---
+++ count down timer/script.js
@@ -0,0 +1,12 @@
+const hoursInputElement = document.getElementById('hoursInput');
+const hoursInputElement = document.getElementById('minutesInput');
+const hoursInputElement = document.getElementById('secondsInput');
+
+
+const hoursOutputElement = document.getElementById('hoursOutput');
+const minutesOutputElement = document.getElementById('minutesOutput');
+const seconds0utputElement = document.getElementById('seconds0utput');
+
+
+
+const buttonElement = document.getElementById('button');
